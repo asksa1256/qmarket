a2b549221b50042d97df568a0f4ab1bad1ee515c
asksa1256@gmail.com <asksa1256@gmail.com>
2025-10-05 00:37:53 +0900

    feat: 로그아웃 추가

--- src/app/features/sign-in-form/model/actions.ts
+++ src/app/features/sign-in-form/model/actions.ts
@@ -1,7 +1,5 @@
 "use server";
 
-import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
 import { createClient } from "@/shared/api/supabase-server-cookie";
 
 export async function login(formData: FormData) {
@@ -24,14 +22,24 @@ export async function login(formData: FormData) {
 export async function signup(formData: FormData) {
   const supabase = await createClient();
 
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
   const data = {
     email: formData.get("email") as string,
     password: formData.get("password") as string,
   };
 
   const { error } = await supabase.auth.signUp(data);
+
+  if (error) {
+    throw error;
+  }
+
+  return { success: true };
+}
+
+export async function logout() {
+  const supabase = await createClient();
+
+  const { error } = await supabase.auth.signOut();
 
   if (error) {
     throw error;
--- src/app/widgets/header/ui/Header.tsx
+++ src/app/widgets/header/ui/Header.tsx
@@ -3,16 +3,32 @@
 import { Button } from "@/shared/ui/button";
 import { useRouter } from "next/navigation";
 import { useUser } from "@/shared/providers/UserProvider";
+import { logout } from "@/features/sign-in-form/model/actions";
+import { toast } from "sonner";
 
 export default function Header() {
   const router = useRouter();
   const user = useUser();
 
+  const handleSignOut = async () => {
+    try {
+      await logout();
+    } catch (error) {
+      console.log(error);
+      toast.error("로그아웃에 실패했습니다. 다시 시도해주세요.");
+    }
+  };
+
   return (
     <header className="py-8 max-w-4xl mx-auto flex items-center">
       <div className="ml-auto">
         {user ? (
-          <span>{user.nickname ?? user.email}</span>
+          <>
+            <span className="text-sm mr-4">
+              <b>{user.nickname ?? user.email}</b>님, 환영합니다.
+            </span>
+            <Button onClick={handleSignOut}>로그아웃</Button>
+          </>
         ) : (
           <Button onClick={() => router.push("/signin")}>로그인</Button>
         )}
